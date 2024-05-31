a81d10e5b647ed4eb056c2eaffd40d5e411754d0
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-05-31 15:26:03 +0300

    Add onSubmit logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,48 @@
+import { useState } from "react";
+
 function App() {
-  return (
-    <main>
-      <Rating />
-    </main>
-  );
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+  function handleFormSubmit() {
+    setIsSubmitted(() => !isSubmitted);
+  }
+
+  return <main>{isSubmitted ? <ThankYou /> : <Rating onSubmit={handleFormSubmit} />}</main>;
 }
 
-function Rating() {
+function Rating({ onSubmit }: { onSubmit: () => void }) {
   return (
-    <section id="Rating">
-      <img src="images/icon-star.svg"></img>
+    <section id="rating">
+      <img src="images/icon-star.svg" className="icon" />
       <h1>How did we do?</h1>
       <p>
         Please let us know how we did with your support request. All feedback is appreciated to help us improve our
         offering!
       </p>
-      <div className="numbers">
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
-        <span>5</span>
-      </div>
-      <button className="btn-submit">submit</button>
+      <form onSubmit={onSubmit}>
+        <div className="numbers">
+          <span>1</span>
+          <span>2</span>
+          <span>3</span>
+          <span>4</span>
+          <span>5</span>
+        </div>
+        <button className="btn-submit">submit</button>
+      </form>
+    </section>
+  );
+}
+
+function ThankYou() {
+  return (
+    <section id="rating" className="thank-you">
+      <img src="images/illustration-thank-you.svg"></img>
+      <p>You selected X out of 5</p>
+      <h2>Thank You</h2>
+      <p>
+        We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in
+        touch!
+      </p>
     </section>
   );
 }
